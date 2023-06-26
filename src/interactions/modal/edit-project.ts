247620a8aa8f0b0aa92d1500069bbcd448c50965
chelluoxou <chelluoxou@gmail.com>
2023-06-26 16:30:22 +0900

    feat: プロジェクトチャンネル名の変更に対応

--- src/interactions/modal/edit-project.ts
+++ src/interactions/modal/edit-project.ts
@@ -3,13 +3,15 @@ import {
   editSessionStore,
   editableProperties,
 } from '../context-menu-command/edit-project'
-import { guildProjectsCache } from '../..'
+import client, { guildProjectsCache } from '../..'
 import { updateProjectPage } from '../../adapters/notion-adapter'
 import { createProjectPostMessage } from '../../utils/create-project'
 import { Logger } from '../../utils/logger'
 import { ProgressReply } from '../../utils/progress-reply'
 
 export async function editProject(interaction: ModalSubmitInteraction) {
+  const guild = client.guilds.cache.get(process.env.DISCORD_GUILD_ID as string)
+
   const logger = new Logger('edit-project')
   const reply = await interaction.reply({
     content: '⏳Applying changes to the project...',
@@ -33,15 +35,17 @@ export async function editProject(interaction: ModalSubmitInteraction) {
   editSessionStore.endSession(interaction.user)
 
   if (projectMessage === undefined) {
+    await progressReply.addProgress(`❌ Failed to get modal session.`)
     throw new Error('Cannot get modal session by user.')
   }
 
   const project: any = guildProjectsCache.getProjectByProjectMessageId(
     projectMessage.id
   )
-
-  logger.log('fetched project:', project);
+  logger.log('fetched project:', project)
   await progressReply.updateProgress('✅ Fetched all projects.')
+
+  const beforeProjectName = project.name ?? ''
 
   for (const inputName of modalInputNames) {
     logger.log('modal input name:', inputName)
@@ -50,11 +54,30 @@ export async function editProject(interaction: ModalSubmitInteraction) {
   }
   logger.log('project:', project)
 
+  if (beforeProjectName !== project.name) {
+    await progressReply.addProgress('⏳ Updating project channel name...')
+    try {
+      console.log('project.discordChannelId:', project.discordChannelId);
+      const projectsChannel = await guild?.channels.fetch(project.discordChannelId)
+      // console.log('projectsChannel:', projectsChannel);
+
+      await projectsChannel?.setName(project.name)
+    } catch (e: any) {
+      logger.error('Error updating project channel name:\n', e)
+      await progressReply.addProgress(
+        `❌ Failed to update project channel name.（${e.message})`
+      )
+      return
+    }
+    logger.log('project channel name updated.')
+    await progressReply.updateProgress('✅ Project channel name updated.')
+  }
+
   await progressReply.addProgress('⏳ Updating notion database...')
-  try{
+  try {
     await updateProjectPage(project)
-  } catch(e: any){
-    logger.error("Project update page error:\n", e)
+  } catch (e: any) {
+    logger.error('Project update page error:\n', e)
     progressReply.addProgress(`❌ Failed to edit project.（$e.message}`)
     return
   }
@@ -69,5 +92,4 @@ export async function editProject(interaction: ModalSubmitInteraction) {
     reply.delete()
     logger.log('reply deleted.')
   }, 8000)
-
 }
