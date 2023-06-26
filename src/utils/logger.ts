3a14d406a913a8ff249290efca88f0a4558ee1f7
chelluoxou <chelluoxou@gmail.com>
2023-06-26 14:26:05 +0900

    feat: error, warn関数の追加

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -8,4 +8,12 @@ export class Logger {
   log(message: string, ...args: any[]) {
     console.log(`[${this.prefix}] ${message}`, ...args)
   }
+
+  error(message: string, ...args: any[]) {
+    console.error(`[${this.prefix}] ${message}`, ...args)
+  }
+
+  warn(message: string, ...args: any[]) {
+    console.warn(`[${this.prefix}] ${message}`, ...args)
+  }
 }
