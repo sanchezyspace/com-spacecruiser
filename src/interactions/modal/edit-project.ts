40e3e0c4b06a5aba32efeb53d728d8f3e762c6e0
chelluoxou <chelluoxou@gmail.com>
2023-06-26 14:21:24 +0900

    feat: 編集時にデータベースをフェッチ、Discord上でエラーがわかるように

--- src/interactions/modal/edit-project.ts
+++ src/interactions/modal/edit-project.ts
@@ -13,8 +13,16 @@ export async function editProject(interaction: ModalSubmitInteraction) {
     ephemeral: true,
   })
 
-  const inputNames = editableProperties.map((e) => e.propName)
-  console.log('inputNames:', inputNames)
+  try {
+    await reply.edit('⏳Fetching project...')
+    await guildProjectsCache.fetchProjects()
+  } catch (e: any) {
+    console.error('Error fetching projects:\n', e)
+    await reply.edit(`❌ Failed to fetch projects ${e.message}`)
+  }
+
+  const modalInputNames = editableProperties.map((e) => e.propName)
+  console.log('inputNames:', modalInputNames)
 
   const projectMessage = editSessionStore.getSession(interaction.user)
   console.log('projectMessage:', projectMessage)
@@ -27,13 +35,23 @@ export async function editProject(interaction: ModalSubmitInteraction) {
   const project: any = guildProjectsCache.getProjectByProjectMessageId(
     projectMessage.id
   )
-  for (const inputName of inputNames) {
-    console.log('inputName:', inputName)
+
+  console.log('fetched project:', project);
+
+
+  for (const inputName of modalInputNames) {
+    console.log('modal input name:', inputName)
     project[inputName] = interaction.fields.getTextInputValue(inputName)
     console.log('project[inputName]:', project[inputName])
   }
   console.log('project:', project)
-  await updateProjectPage(project)
+  try{
+    await updateProjectPage(project)
+  } catch(e: any){
+    console.error("Project update page error:\n", e)
+    reply.edit(`❌ Failed to edit project.（$e.message}`)
+    return
+  }
   console.log('project updated.')
   reply.edit('✅ Project edited successfully.')
   projectMessage.edit(createProjectPostMessage(project))
