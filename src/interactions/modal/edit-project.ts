bff5d003f03611709f6314ccccc3469d460395f3
chelluoxou <chelluoxou@gmail.com>
2023-06-26 14:26:31 +0900

    feat: ロガーの追加

--- src/interactions/modal/edit-project.ts
+++ src/interactions/modal/edit-project.ts
@@ -6,8 +6,11 @@ import {
 import { guildProjectsCache } from '../..'
 import { updateProjectPage } from '../../adapters/notion-adapter'
 import { createProjectPostMessage } from '../../utils/create-project'
+import { Logger } from '../../utils/logger'
 
 export async function editProject(interaction: ModalSubmitInteraction) {
+  const logger = new Logger('edit-project')
+
   const reply = await interaction.reply({
     content: '⏳Applying changes to the project...',
     ephemeral: true,
@@ -17,15 +20,15 @@ export async function editProject(interaction: ModalSubmitInteraction) {
     await reply.edit('⏳Fetching project...')
     await guildProjectsCache.fetchProjects()
   } catch (e: any) {
-    console.error('Error fetching projects:\n', e)
+    logger.error('Error fetching projects:\n', e)
     await reply.edit(`❌ Failed to fetch projects ${e.message}`)
   }
 
   const modalInputNames = editableProperties.map((e) => e.propName)
-  console.log('inputNames:', modalInputNames)
+  logger.log('inputNames:', modalInputNames)
 
   const projectMessage = editSessionStore.getSession(interaction.user)
-  console.log('projectMessage:', projectMessage)
+  logger.log('projectMessage:', projectMessage)
   editSessionStore.endSession(interaction.user)
 
   if (projectMessage === undefined) {
@@ -36,23 +39,22 @@ export async function editProject(interaction: ModalSubmitInteraction) {
     projectMessage.id
   )
 
-  console.log('fetched project:', project);
-
+  logger.log('fetched project:', project);
 
   for (const inputName of modalInputNames) {
-    console.log('modal input name:', inputName)
+    logger.log('modal input name:', inputName)
     project[inputName] = interaction.fields.getTextInputValue(inputName)
-    console.log('project[inputName]:', project[inputName])
+    logger.log('project[inputName]:', project[inputName])
   }
-  console.log('project:', project)
+  logger.log('project:', project)
   try{
     await updateProjectPage(project)
   } catch(e: any){
-    console.error("Project update page error:\n", e)
+    logger.error("Project update page error:\n", e)
     reply.edit(`❌ Failed to edit project.（$e.message}`)
     return
   }
-  console.log('project updated.')
+  logger.log('project updated.')
   reply.edit('✅ Project edited successfully.')
   projectMessage.edit(createProjectPostMessage(project))
 }
